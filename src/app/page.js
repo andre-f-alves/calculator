85b1c38743f862f50bc3d33ea17fc5bb7d258a5f
André Alves <112015245+andre-f-alves@users.noreply.github.com>
2023-07-23 21:04:32 -0300

    Criação e refatoração do projeto

--- src/app/page.js
+++ src/app/page.js
@@ -1,94 +1,120 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
+import './main-style.css'
+import { useState } from 'react'
+
+
+const Screen = ({ value, result }) => {
+  return (
+    <div className='screen'>
+      <span className='operation-screen'>{value}</span>
+      <span className='result-screen'>{result}</span>
+    </div>
+  )
+}
+
+const ButtonCalc = ({ label, fnOnClick, especialId='' }) => {
+  return (
+    <button type='button' id={especialId} className='btn-calc' onClick={fnOnClick}>{label}</button>
+  )
+}
 
 export default function Home() {
+  const [ screenValue, setScreenValue ] = useState('')
+  const [ result, setResult ] = useState(0)
+  const [ validators, setValidators ] = useState({
+    preventOperator: true,
+    preventDecimalPoint: true,
+    isOperated: false
+  })
+
+  const operatorsList = ['+', '-', '*', '/']
+
+  const showDigitOnScreen = (digit) => {
+    let validatedValue = ''
+
+    if (screenValue === '') {
+      if (!isNaN(digit) || digit === '-') {
+        validatedValue = digit
+        setValidators({...validators, preventOperator: digit === '-' ? false : true})
+      }
+    
+    } else if (screenValue.length >= 1) {
+      if (!isNaN(digit)) {
+        validatedValue = digit
+        setValidators({...validators, preventOperator: true}) 
+      
+      } else if (operatorsList.includes(digit) && validators.preventOperator && !isNaN(screenValue[screenValue.length - 1])) {
+        validatedValue = digit
+        setValidators({...validators, preventOperator: false, preventDecimalPoint: true})
+      
+      } else if (digit === '.' && validators.preventDecimalPoint && !isNaN(screenValue[screenValue.length - 1])) {
+        validatedValue = digit
+        setValidators({...validators, preventDecimalPoint: false})
+      }
+    }
+
+    setScreenValue(validators.isOperated ? result + validatedValue : screenValue + validatedValue)
+    if (validators.isOperated) {
+      setValidators({...validators, isOperated: false})
+    }
+  }
+
+  const clearMemory = () => {
+    setValidators({...validators, preventOperator: true, preventDecimalPoint: true, isOperated: false})
+    setScreenValue('')
+    setResult(0)
+  }
+
+  const mathOperation = (operation) => {
+    if (operation === 'backspace') {
+      let value = screenValue
+      value = value.substring(0, value.length - 1)
+      setScreenValue(value)
+      setValidators({...validators, isOperated: false})
+      
+    } else {
+      try {
+        let expression = screenValue
+  
+        if (isNaN(expression[expression.length - 1])) {
+          expression = expression.substring(0, expression.length - 1)
+          setScreenValue(expression)
+        }
+        
+        const operationResult = eval(expression ? expression : 0)
+        setResult(operationResult)
+        setValidators({...validators, isOperated: true})
+  
+      } catch {
+        setResult('ERRO')
+      }
+    }
+  }
+
+  const labels = ['C', '<', '/', '*', '7', '8', '9', '-', '4', '5', '6', '+', '1', '2', '3', '=', '0', '.']
+
+  const calcKeys = labels.map(label => {
+    if (label === 'C') {
+      return <ButtonCalc key={label} label={label} fnOnClick={() => clearMemory()}/>
+
+    } else if (label === '<') {
+      return <ButtonCalc key={label} label={label} fnOnClick={() => mathOperation('backspace')}/>
+
+    } else if (label === '=') {
+      return <ButtonCalc key={label} label={label} fnOnClick={() => mathOperation('=')} especialId='equal'/>
+    }
+
+    return <ButtonCalc key={label} label={label} fnOnClick={() => showDigitOnScreen(label)} especialId={label === '0' ? 'zero' : ''}/>
+  })
+  
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className='calc'>
+      <div className='container'>
+        <h3>Calculadora</h3>
+        <Screen value={screenValue} result={result}/>
+        <div className='buttons'>
+          {calcKeys}
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   )
